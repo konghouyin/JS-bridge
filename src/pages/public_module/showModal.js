abf04d52d8a735c6565243febc3ea2dcb8f09f29
konghouyin <1360234119@qq.com>
2019-07-23 20:45:13 +0800

    系统弹窗接口

--- src/pages/public_module/showModal.js
+++ src/pages/public_module/showModal.js
@@ -1,21 +1,47 @@
-import {addCallback} from './callbackPool'
+import {
+    addCallback
+} from './callbackPool'
 
-export default function (obj) {
+export default function(obj) {
     let callbackId = Math.floor(Math.random() * 2000000000)
     let type = 'showModal'
     let send = {
         type: type,
         data: {
-            title:"",
-            content:"asd",
-            showCancel:true,
-            cancelText:"abc",
-            cancelColor:"#f40",
-            confirmText:"dsf",
-            confirmColor:"#f40"
+            title: '',
+            showTitle: false,
+            content: '',
+            showCancel: true,
+            cancelText: '取消',
+            cancelColor: '#000000',
+            confirmText: '确定',
+            confirmColor: '#576B95'
         },
         callbackId: callbackId
     }
+    if (obj.title != undefined) {
+        send.data.title = obj.title
+        send.data.showtitle = true
+    }
+    if (obj.content != undefined) {
+        send.data.content = obj.content
+    }
+    if (obj.showCancel != undefined) {
+        send.data.showCancel = obj.showCancel
+    }
+    if (obj.cancelText != undefined) {
+        send.data.cancelText = obj.cancelText
+    }
+    if (obj.cancelColor != undefined) {
+        send.data.cancelColor = obj.cancelColor
+    }
+    if (obj.confirmText != undefined) {
+        send.data.confirmText = obj.confirmText
+    }
+    if (obj.confirmColor != undefined) {
+        send.data.confirmColor = obj.confirmColor
+    }
+
     addCallback(this, callbackId, type, obj.success, obj.fail, obj.complete)
     this.jsCallNative(send)
 }
